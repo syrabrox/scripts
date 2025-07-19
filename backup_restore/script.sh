2db637ac172d88f05689378f6a3d13c52f33dff5
AsA <102066638+syrabrox@users.noreply.github.com>
2025-07-19 11:12:09 +0200

    Update script.sh

--- backup_restore/script.sh
+++ backup_restore/script.sh
@@ -4,6 +4,7 @@ set -e
 LOCKFILE="/tmp/backup.lock"
 BACKUP_DIR="/backup"
 WEBHOOK_FILE="$BACKUP_DIR/webhook.txt"
+echo $WEBHOOK_FILE
 DATE=$(date +'%Y-%m-%d_%H-%M-%S')
 
 if [ -f "$WEBHOOK_FILE" ]; then
